5e19bf7eb19332b6254150ed573d364d19f42352
Jesús Israel Pantoja Zavala <49212495+JIPZ00@users.noreply.github.com>
2021-12-06 20:09:10 -0600

    Notas
    Creacion de notas esta funcional.
    Falta editar y eliminar notas.

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -1,9 +1,14 @@
+// ignore_for_file: unnecessary_const
+
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:list/nota.dart';
 import 'package:list/user_pref.dart';
 import 'package:list/values/tema.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:list/widgets/notes_card.dart';
+import 'dart:math';
+
 // import 'models/nota.dart';
 import 'package:list/widgets/note_modal.dart';
 
@@ -19,99 +24,31 @@ class FirstPage extends StatefulWidget {
 class _FirstPageState extends State<FirstPage>
     with SingleTickerProviderStateMixin {
   late TabController _tabController;
-
   CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
 
   int tabIndex = 0;
 
+  List<Map<String, dynamic>> notesList = [];
+
+  Future<List<Map<String, dynamic>>> future = Nota.getUserNotes();
   @override
   void initState() {
     _tabController = TabController(vsync: this, length: 3);
     super.initState();
   }
 
-  // Lista de notas que se obtiene del frontEnd ( De momento está fija)
-  final List<Map<String, dynamic>> notesList = [
-    {
-      'title': 'Nota 1',
-      'date': DateTime.now(),
-      'content':
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sagittis.',
-      'color': const Color(0xff6270C4)
-    },
-    {
-      'title': 'Nota 2',
-      'date': DateTime.now(),
-      'content':
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consectetur neque nec pulvinar blandit. Donec sed arcu vestibulum, feugiat enim sed, commodo tellus. Nunc id interdum leo. Sed erat orci, porttitor nec ipsum et, egestas efficitur mi. Nulla quam felis, ultrices non efficitur quis, laoreet venenatis augue. Nulla vitae laoreet orci. Vestibulum viverra, urna ullamcorper pretium fermentum, mi mauris convallis tortor, id pulvinar turpis eros vitae metus. In sed urna aliquam, iaculis orci ut, ultrices magna. Praesent eleifend, sem sit amet fringilla dapibus, magna nisl scelerisque tellus, malesuada auctor sapien arcu et magna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas nisi lectus, commodo in auctor nec, convallis in augue. Duis ornare arcu augue. Donec congue ornare ante, eget mattis velit bibendum at.',
-      'color': const Color(0xff5BC475)
-    },
-    {
-      'title': 'Nota 3',
-      'date': DateTime.now(),
-      'content':
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pulvinar, ligula nec molestie venenatis, nulla velit rutrum metus, ut pharetra.',
-      'color': const Color(0xff00ADB5)
-    },
-    {
-      'title': 'Nota 4',
-      'date': DateTime.now(),
-      'content':
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sagittis.',
-      'color': const Color(0xff5BC475)
-    },
-    {
-      'title': 'Nota 5',
-      'date': DateTime.now(),
-      'content':
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sagittis.',
-      'color': const Color(0xff00ADB5)
-    },
-    {
-      'title': 'Nota 3',
-      'date': DateTime.now(),
-      'content':
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pulvinar, ligula nec molestie venenatis, nulla velit rutrum metus, ut pharetra.',
-      'color': const Color(0xff00ADB5)
-    },
-    {
-      'title': 'Nota 4',
-      'date': DateTime.now(),
-      'content':
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sagittis.',
-      'color': const Color(0xff5BC475)
-    },
-    {
-      'title': 'Nota 5',
-      'date': DateTime.now(),
-      'content':
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sagittis.',
-      'color': const Color(0xff00ADB5)
-    },
-    {
-      'title': 'Nota 3',
-      'date': DateTime.now(),
-      'content':
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pulvinar, ligula nec molestie venenatis, nulla velit rutrum metus, ut pharetra.',
-      'color': const Color(0xff00ADB5)
-    },
-    {
-      'title': 'Nota 4',
-      'date': DateTime.now(),
-      'content':
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sagittis.',
-      'color': const Color(0xff5BC475)
-    },
-    {
-      'title': 'Nota 5',
-      'date': DateTime.now(),
-      'content':
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sagittis.',
-      'color': const Color(0xff00ADB5)
-    },
-  ];
+  // Función para obtener un color de manera aleatoria
+  Color getColor() {
+    var rng = Random();
+    var colorNumber = rng.nextInt(2);
+    if (colorNumber == 0) {
+      return const Color(0xff6270C4);
+    } else {
+      return const Color(0xff5BC475);
+    }
+  }
 
   // Función para obtener el widget con la sección de notas
   Widget getNotesSection(BuildContext context) {
@@ -123,10 +60,13 @@ class _FirstPageState extends State<FirstPage>
         itemBuilder: (BuildContext context, index) {
           final item = notesList[index];
           return NoteCard(
-              title: item['title'],
-              content: item['content'],
-              date: item['date'],
-              cardColor: item['color']);
+            title: item['titulo'],
+            content: item['contenido'],
+            date: item['fecha'].toDate(),
+            cardColor: getColor(),
+            index: index,
+            callbackNoteClick: callbackNoteClick,
+          );
         });
   }
 
@@ -170,6 +110,103 @@ class _FirstPageState extends State<FirstPage>
     return const Center(child: Text('Sección de tareas'));
   }
 
+// Función para obtener el scaffold de la pagina
+  Widget getPageScaffold() {
+    return Scaffold(
+      appBar: AppBar(),
+      backgroundColor: backgroundPageColor,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: primaryBlueColor,
+        child: const Icon(Icons.add, color: primaryWhite, size: 30.0),
+        onPressed: () {
+          switch (_tabController.index) {
+            // El indice está en la pantalla de notas
+            case 0:
+              print("Creando nueva nota");
+              showModalBottomSheet(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return NoteModal(
+                        edit: false, noteCallback: callbackNewNote);
+                  });
+              break;
+            // El índice está en la pantalla del calendario
+            case 1:
+              print("Creando nuevo evento");
+              break;
+            case 2:
+              // El índice está en la pantalla de cosas x hacer
+              print("Creando nueva tarea");
+              break;
+          }
+        },
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          getNotesSection(context),
+          getCalendarSection(context),
+          getToDoSection(context),
+        ],
+      ),
+      bottomNavigationBar: TabBar(
+        controller: _tabController,
+        indicatorColor: primaryBlueColor,
+        labelColor: primaryBlueColor,
+        unselectedLabelColor: unselectedOptionColor,
+        tabs: const [
+          Tab(
+            icon: Icon(Icons.note_outlined, size: 30.0),
+            text: "Notes",
+          ),
+          Tab(
+            icon: Icon(Icons.calendar_today_rounded, size: 30.0),
+            text: "Calendar",
+          ),
+          Tab(
+            icon: Icon(Icons.view_list_rounded, size: 30.0),
+            text: "Things to do",
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Función para obtener el widget con la animación de carga
+  Widget getFetchAnimation() {
+    return const Scaffold(
+        body: Center(
+            child: CircularProgressIndicator(
+      strokeWidth: 3,
+    )));
+  }
+
+// Función que devuelve la pantalla con un mensaje de error en caso de que no se haya podido obtener la información del usuario
+  Widget getErrorMessage() {
+    return const Scaffold(
+        body: const Center(
+            child: Padding(
+      padding: EdgeInsets.all(8.0),
+      child: const Text(
+        'Error al obtener la información del usuario',
+        style: TextStyle(
+            fontSize: 30, fontWeight: FontWeight.w600, color: Colors.red),
+      ),
+    )));
+  }
+
+  // Método Callback que se dispara cuando el usuario guarde una nueva nota
+  callbackNewNote() {
+    print("Cambió la lista de notas");
+    setState(() {
+      future = Nota.getUserNotes();
+    });
+  }
+  // Método callback que se dispara cuando el usuario de click a una nota ya creada
+  callbackNoteClick(int index){
+    print("Nota seleccionada: "+ notesList[index].toString());
+  }
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -179,102 +216,57 @@ class _FirstPageState extends State<FirstPage>
         primarySwatch: Colors.blue,
       ),
       home: SafeArea(
-        child: Scaffold(
-          backgroundColor: backgroundPageColor,
-          floatingActionButton: FloatingActionButton(
-            backgroundColor: primaryBlueColor,
-            child: const Icon(Icons.add, color: primaryWhite, size: 30.0),
-            onPressed: () async {
-              switch (_tabController.index) {
-                // El indice está en la pantalla de notas
-                case 0:
-                  print("Creando nueva nota");
-                  print(await UserPref.getID());
-                  showModalBottomSheet(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return const NoteModal();
-                      });
-                  break;
-                // El índice está en la pantalla del calendario
-                case 1:
-                  print("Creando nuevo evento");
-                  break;
-                case 2:
-                  // El índice está en la pantalla de cosas x hacer
-                  print("Creando nueva tarea");
-                  break;
-              }
-            },
-          ),
-          body: TabBarView(
-            controller: _tabController,
-            children: [
-              getNotesSection(context),
-              getCalendarSection(context),
-              getToDoSection(context),
-            ],
-          ),
-          bottomNavigationBar: TabBar(
-            controller: _tabController,
-            indicatorColor: primaryBlueColor,
-            labelColor: primaryBlueColor,
-            unselectedLabelColor: unselectedOptionColor,
-            tabs: const [
-              Tab(
-                icon: Icon(Icons.note_outlined, size: 30.0),
-                text: "Notes",
-              ),
-              Tab(
-                icon: Icon(Icons.calendar_today_rounded, size: 30.0),
-                text: "Calendar",
-              ),
-              Tab(
-                icon: Icon(Icons.view_list_rounded, size: 30.0),
-                text: "Things to do",
-              ),
-            ],
-          ),
-        ),
-      ),
+          child: FutureBuilder<List<Map<String, dynamic>>>(
+        future: future,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            notesList = snapshot.data!;
+            return getPageScaffold();
+          } else if (snapshot.hasError) {
+            return getErrorMessage();
+          } else {
+            return getFetchAnimation();
+          }
+        },
+      )),
     );
   }
 }
 
-class Note {
-  int id;
-  String title;
-  String content;
-  DateTime date_created;
-  DateTime date_last_edited;
-  Color note_color;
-  int is_archived = 0;
+// class Note {
+//   int id;
+//   String title;
+//   String content;
+//   DateTime date_created;
+//   DateTime date_last_edited;
+//   Color note_color;
+//   int is_archived = 0;
 
-  Note(this.id, this.title, this.content, this.date_created,
-      this.date_last_edited, this.note_color);
+//   Note(this.id, this.title, this.content, this.date_created,
+//       this.date_last_edited, this.note_color);
 
-  Map<String, dynamic> toMap(bool forUpdate) {
-    var data = {
-//      'id': id,  since id is auto incremented in the database we don't need to send it to the insert query.
-      'title': utf8.encode(title),
-      'content': utf8.encode(content),
-      'date_created': epochFromDate(date_created),
-      'date_last_edited': epochFromDate(date_last_edited),
-      'note_color': note_color.value,
-      'is_archived': is_archived //  for later use for integrating archiving
-    };
-    if (forUpdate) {
-      data["id"] = this.id;
-    }
-    return data;
-  }
+//   Map<String, dynamic> toMap(bool forUpdate) {
+//     var data = {
+// //      'id': id,  since id is auto incremented in the database we don't need to send it to the insert query.
+//       'title': utf8.encode(title),
+//       'content': utf8.encode(content),
+//       'date_created': epochFromDate(date_created),
+//       'date_last_edited': epochFromDate(date_last_edited),
+//       'note_color': note_color.value,
+//       'is_archived': is_archived //  for later use for integrating archiving
+//     };
+//     if (forUpdate) {
+//       data["id"] = this.id;
+//     }
+//     return data;
+//   }
 
-// Converting the date time object into int representing seconds passed after midnight 1st Jan, 1970 UTC
-  int epochFromDate(DateTime dt) {
-    return dt.millisecondsSinceEpoch ~/ 1000;
-  }
+// // Converting the date time object into int representing seconds passed after midnight 1st Jan, 1970 UTC
+//   int epochFromDate(DateTime dt) {
+//     return dt.millisecondsSinceEpoch ~/ 1000;
+//   }
 
-  void archiveThisNote() {
-    is_archived = 1;
-  }
-}
+//   void archiveThisNote() {
+//     is_archived = 1;
+//   }
+// }
--- lib/widgets/notes_card.dart
+++ lib/widgets/notes_card.dart
@@ -5,52 +5,63 @@ class NoteCard extends StatelessWidget {
   final String title;
   final String content;
   final DateTime date;
+  final int index;
   final Color cardColor;
+  // Callback que se va a ejecutar cuando se le de click a una nota
+  final callbackNoteClick;
   // Constructor que recibe el titulo de la nota, el contenido y la fecha
   const NoteCard(
       {Key? key,
       required this.title,
       required this.content,
       required this.date,
-      required this.cardColor})
+      required this.cardColor,
+      required this.index,
+      required this.callbackNoteClick})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
-        elevation: 10,
-        color: cardColor,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Fecha de la nota
-              Expanded(
-                  flex: 1,
-                  child: Text(DateFormat('dd-MM-yyyy').format(date),
-                      style: const TextStyle(
-                          color: Color(0xffFFFFFF), fontSize: 16))),
-              // Titulo de la nota
-              Expanded(
-                  flex: 2,
-                  child: Text(title,
-                      style: const TextStyle(
-                          color: Color(0xffFFFFFF),
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600))),
-              // Contenido
-              Expanded(
-                  flex: 7,
-                  child: Text(content,
-                      maxLines: 5,
-                      style: const TextStyle(
-                          overflow: TextOverflow.ellipsis,
-                          color: Color(0xffFFFFFF),
-                          fontSize: 14))),
-            ],
-          ),
-        ));
+    return GestureDetector(
+      onTap: (){
+        // Se presionó la tarjeta, se ejecuta el callback
+        callbackNoteClick(index);
+      },
+      child: Card(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
+          elevation: 10,
+          color: cardColor,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Fecha de la nota
+                Expanded(
+                    flex: 1,
+                    child: Text(DateFormat('dd-MM-yyyy').format(date),
+                        style: const TextStyle(
+                            color: Color(0xffFFFFFF), fontSize: 16))),
+                // Titulo de la nota
+                Expanded(
+                    flex: 2,
+                    child: Text(title,
+                        style: const TextStyle(
+                            color: Color(0xffFFFFFF),
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600))),
+                // Contenido
+                Expanded(
+                    flex: 7,
+                    child: Text(content,
+                        maxLines: 5,
+                        style: const TextStyle(
+                            overflow: TextOverflow.ellipsis,
+                            color: Color(0xffFFFFFF),
+                            fontSize: 14))),
+              ],
+            ),
+          )),
+    );
   }
 }
